bf190fbac4a27ec59f38510a3c8a9c79ecead09c
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-20 16:19:56 -0500

    change backup settings to be a self contained full backup script

--- vscode/backup-settings.sh
+++ vscode/backup-settings.sh
@@ -1,4 +1,34 @@
 #!/bin/bash
 
+TEMP_DIR=$HOME/crouton-install-temp
+
+# clean up if things are left over for some reason
+if [ -d "$TEMP_DIR" ]; then
+  rm -rf $TEMP_DIR
+fi
+
+# create temporary directory
+mkdir $TEMP_DIR
+cd $TEMP_DIR
+
+# clone repository
+git clone https://github.com/mzrimsek/crouton-init.git
+cd crouton-init/vscode
+
+# backup settings and snippets
 cp $HOME/.config/Code/User/settings.json settings.json
 cp $HOME/.config/Code/User/snippets/typescript.json snippets/typescript.json
+
+# backup extensions
+cd extensions
+./backup-extensions.sh
+
+GIT_STATUS=$(git status --short)
+if [ "$GIT_STATUS" != "" ]; then
+  git commit -am "Update extensions"
+  git push
+fi
+
+# clean up
+cd $HOME
+rm -rf $TEMP_DIR
--- vscode/extensions/backup-extensions.sh
+++ vscode/extensions/backup-extensions.sh
@@ -1,9 +1,3 @@
 cat <<EOT > extensions.txt
 `code --list-extensions`
 EOT
-
-GIT_STATUS=$(git status --short)
-if [ "$GIT_STATUS" != "" ]; then
-  git commit -am "Update extensions"
-  git push
-fi
