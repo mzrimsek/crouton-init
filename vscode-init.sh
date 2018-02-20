e11a79759f0262cab00b1df1a93779a0b5b16745
Mike Zrimsek <mikezrimsek@gmail.com>
2018-02-20 16:13:33 -0500

    vscode init should work a bit better now

--- vscode-init.sh
+++ vscode-init.sh
@@ -2,7 +2,7 @@
 
 TEMP_DIR=$HOME/crouton-install-temp
 
-# clean up if things are left over for smoe reason
+# clean up if things are left over for some reason
 if [ -d "$TEMP_DIR" ]; then
   rm -rf $TEMP_DIR
 fi
@@ -11,16 +11,17 @@ fi
 mkdir $TEMP_DIR
 cd $TEMP_DIR
 
-# restore extensions
-wget https://raw.githubusercontent.com/mzrimsek/crouton-init/master/vscode/extensions/extensions.txt
-wget https://raw.githubusercontent.com/mzrimsek/crouton-init/master/vscode/extensions/restore-extensions.sh -O - | sh
+# clone repository
+git clone https://github.com/mzrimsek/crouton-init.git
+cd crouton-init/vscode
 
 # restore user settings and snippets
-wget https://raw.githubusercontent.com/mzrimsek/crouton-init/master/vscode/settings.json
 mv settings.json $HOME/.config/Code/User
+mv snippets/typescript.json $HOME/.config/Code/User/snippets
 
-wget https://raw.githubusercontent.com/mzrimsek/crouton-init/master/vscode/snippets/typescript.json
-mv typescript.json $HOME/.config/Code/User/snippets
+# restore extensions
+cd extensions
+./restore-extensions.sh
 
 # clean up
 cd $HOME
